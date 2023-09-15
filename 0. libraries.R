d3aab4f30b238526c7858202fabb9c264d407cde
TomSOWI <tomklotz@gmx.net>
2023-09-15 17:14:31 +0200

    splithalf update

---
+++ 0. libraries.R
@@ -0,0 +1,12 @@
+###
+library(quanteda)
+library(quanteda.textplots)
+library(tidyverse)
+library(ggplot2)
+library(reshape2)
+library(readr)
+library(lubridate)
+library(splithalfr)
+devtools::install_github("jogrue/regexhelpeR")
+devtools::install_github("jogrue/multidictR")
+devtools::install_github("jogrue/popdictR")
--- libraries.R
+++
@@ -1,8 +0,0 @@
-###
-library(quanteda)
-library(quanteda.textplots)
-library(tidyverse)
-library(ggplot2)
-library(reshape2)
-library(readr)
-library(lubridate)
